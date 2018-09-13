b2bbfd25c5cf9e951b615a8fcdc4818acb285654
Breno Alberto <brenoaps435@gmail.com>
2018-09-13 13:59:23 -0300

    seeding the db

---
+++ seed.js
@@ -0,0 +1,40 @@
+var mongoose = require("mongoose"),
+    User = require("./models/user");
+
+var data = [
+    {
+        name: 'Bob Lee',
+        email: 'boblee@gmail.com',
+        cpf: '52865518078'
+    },
+    {
+        name: 'George Kimmel',
+        email: 'georgek@hotmail.com',
+        cpf: '04331388077'
+    },
+    {
+        name: 'Homer Simpsons',
+        email: 'homero@gmail.com',
+        cpf: '22263474021'
+    }
+];
+
+function seedDB(){
+    User.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed user");
+        data.forEach(function(seed){
+            User.create(seed, function(err, user){
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("added user");
+                }
+            })
+        })
+    })
+}
+
+module.exports = seedDB;
