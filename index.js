ce265ff048214181ec2e975cc7cbdd07d6194c2a
Breno Alberto <brenoaps435@gmail.com>
2018-09-13 14:00:27 -0300

    general update

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-var express  = require("express"),
+var express = require("express"),
+    app     = express(),
+    bodyParser  = require("body-parser"),
     mongoose = require("mongoose"),
-    flash    = require("connect-flash"),
-    User     = require("user");
+    flash = require("connect-flash"),
+    seedDB = require("./seed"),
+    User = require("./models/user"),
+    methodOverride = require("method-override");
+
+require('dotenv').load();
 
 var userRoutes = require("./routes/users");
 
+mongoose.Promise = global.Promise;
+
+const databaseUri = 'mongodb://localhost/DuoREST';
+
+mongoose.connect(databaseUri, { useNewUrlParser: true })
+    .then(() => console.log(`Database connected`))
+    .catch(err => console.log(`Database connection error: ${err.message}`));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+seedDB();
+
 app.use("/", userRoutes);
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("The Server Has Started!");
 });
