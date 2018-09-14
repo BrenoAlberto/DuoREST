d3a1e97be6136e6708b550d5296223c828796a9c
Breno Alberto <brenoaps435@gmail.com>
2018-09-14 02:35:02 -0300

    search update

--- public/search.js
+++ public/search.js
@@ -8,12 +8,23 @@ $('#user-search').on('input', function() {
       data.forEach(function(user) {
         $('#user-grid').append(`
         <div class="col-md-3 col-sm-6">
-            <div class="container">
-                <h4>${ user.name }</h4>
-                <h4>${ user.email }</h4>
-                <h4>${ user.cpf }</h4>
+        <div class="card">
+            <h3>${ user.name }</h3>
+            <hr>
+            <h4>${ user.email }</h4>
+            <hr>
+            <h4>${ user.cpf }</h4>
+            <hr>
+            <div class="row text-center">
+                <div class="col-md-6 col-sm-6">
+                    <a class="btn btn-warning" href="/users/${ user._id }/edit">Editar</a>
+                </div>
+                <form action="/users/${ user._id }?_method=DELETE" method="POST" class="delete-form col-md-6 col-sm-6">
+                    <button class="btn btn-danger">Deletar</button>
+                </form>
             </div>
         </div>
+    </div>
         `);
       });
     });
