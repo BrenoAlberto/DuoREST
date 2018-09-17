58f10580e676d49c3c39319bb9a748472e48f6ba
Breno Alberto <brenoaps435@gmail.com>
2018-09-16 21:07:50 -0300

    models refactor

--- models/adm.js
+++ models/adm.js
@@ -2,11 +2,11 @@ var mongoose = require("mongoose"),
     bcrypt = require('bcrypt');
 
 var AdmSchema = new mongoose.Schema({
-    username: String,
-    hash_password: String
+    username: { type: String, required: true, unique: true },
+    hash_password: { type: String, required: true }
 });
 
-AdmSchema.methods.comparePassword = function(password){
+AdmSchema.methods.comparePassword = function (password) {
     return bcrypt.compareSync(password, this.hash_password);
 };
 
--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    cpf: Number
+    name: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/
+    },
+    cpf: {
+        type: Number,
+        required: true,
+        unique: true,
+        match: /^(d{3}.d{3}.d{3}-d{2})|(d{11})$/
+    }
 });
 
 module.exports = mongoose.model("User", UserSchema);
