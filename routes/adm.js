e51e60b258f8a6226f3d79af741624a0c279027e
Breno Alberto <brenoaps435@gmail.com>
2018-09-16 21:07:37 -0300

    routing refactor

--- routes/adm.js
+++ routes/adm.js
@@ -1,29 +1,15 @@
 var express = require("express"),
     router = express.Router(),
-    Adm = require("../models/adm");
+    admHandlers = require("../controllers/adm");
 
-router.get("/", function (req, res) {
-    res.render("users/login");
-});
+router.route("")
+    .get(admHandlers.loginPage);
 
-router.post("/login", function (req, res) {
-    Adm.findOne({
-        username: req.body.username
-    }, function (err, adm) {
-        if (err) {
-            console.log(err);
-        }
-        if (!adm) {
-            res.status(401).json({ message: 'Usuário não encontrado' });
-        } else if (adm) {
-            if (adm.comparePassword(req.body.password)) {
-                res.status(401).json({ message: 'Senha errada' });
-            } else {
-                res.redirect("/users");
-                return res.json({ token: jwt.sign({ username: adm.username, _id: adm._id }, 'RESTFULAPI') });
-            }
-        }
-    })
-});
+router.route("")
+    .post(admHandlers.login);
+
+router.route("/register")
+    .get(admHandlers.registerPage)
+    .post(admHandlers.register);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,101 +1,21 @@
 var express = require("express"),
     router = express.Router(),
-    User = require("../models/user"),
-    Adm  = require("./adm"),
-    middleware = require("../middleware"),
-    { loginRequired } = middleware;
+    userHandlers  = require("../controllers/user"),
+    admHandlers = require("../controllers/adm"),
+    checkAuth = require('../middleware/check-auth');
 
-// Define escapeRegex function for search feature
-function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
+router.route('')
+    .get(checkAuth, userHandlers.showUsers)
+    .post( userHandlers.createUser);
 
-
-//INDEX - show all users
-router.get("/", loginRequired, function (req, res) {
-    if (req.query.search && req.xhr) {
-        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-        User.find({ name: regex }, function (err, allUsers) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.status(200).json(allUsers);
-            }
-        });
-    } else {
-        User.find({}, function (err, allUsers) {
-            if (err) {
-                console.log(err);
-            } else {
-                if (req.xhr) {
-                    res.json(allUsers);
-                } else {
-                    res.render("users/index", { users: allUsers, page: 'users' });
-                }
-            }
-        });
-    }
-});
-
-//CREATE - add new user to DB
-router.post("/", loginRequired, function (req, res) {
-    var name = req.body.name;
-    var email = req.body.email;
-    var cpf = req.body.cpf;
-    var newUser = { name: name, email: email, cpf: cpf };
-    User.create(newUser, function (err, newlyCreated) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(newlyCreated);
-            res.redirect("/");
-        }
-    });
-});
-
-//NEW - show form to create new user
-router.get("/new",loginRequired, function (req, res) {
-    res.render("users/new");
-});
-
-// EDIT - shows edit form for a user
-router.get("/:id/edit", loginRequired, function (req, res) {
-    User.findById(req.params.id).exec(function(err, foundUser){
-        if(err || !foundUser){
-            console.log(err);
-            //req.flash('error', 'Desculpe mas este usuário não existe!');
-            return res.redirect('/users');
-        }
-        console.log(foundUser);
-        res.render("users/edit", { user: foundUser });
-    })
+router.route("/new")
+    .get(  userHandlers.createUserForm);
     
-});
+router.route("/:id/edit")
+    .get(  userHandlers.editUserForm);
 
-// PUT - updates user in the database
-router.put("/:id", loginRequired, function (req, res) {
-    var newData = { name: req.body.name, email: req.body.email, cpf: req.body.cpf };
-    User.findByIdAndUpdate(req.params.id, { $set: newData }, function (err, user) {
-        if (err) {
-            //req.flash("error", err.message);
-            res.redirect("back");
-        } else {
-            //req.flash("success", "Atualizado com sucesso!");
-            res.redirect("/users/" + user._id);
-        }
-    });
-});
-
-// DELETE - removes user from the database
-router.delete("/:id", loginRequired, function (req, res) {
-    User.findByIdAndRemove(req.params.id).exec(function (err) {
-        if (err) {
-            //req.flash('error', err.message);
-            return res.redirect('/');
-        }
-        //req.flash('error', 'Usuário deletado!');
-        res.redirect('/users');
-    });
-});
+router.route("/:id")
+    .put(  userHandlers.updateUser)
+    .delete( userHandlers.deleteUser);
 
 module.exports = router;
