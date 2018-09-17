88542e2c0bed599f35e0a53f74b52ae52b20a56e
Breno Alberto <brenoaps435@gmail.com>
2018-09-17 16:23:46 -0300

    general update

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try{
-        const token = req.cookies.Authorization;
+        const token = req.cookies.Authorization.split(' ')[1];
         const decoded = jwt.verify(token, 'secret');
         req.admData = decoded;
         next();
